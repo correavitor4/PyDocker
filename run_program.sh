010567bf3b11a2c2a90f11f066e2d5d29fa7b627
vitor-laptop-fedora <correavitor4@hotmail.com>
2026-03-27 12:30:54 -0300

    open.sh

---
+++ run_program.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Criar e ativar o ambiente virtual
+python -m venv .venv
+source .venv/bin/activate
+
+# Instalar as dependências
+python -m pip install --upgrade pip
+python -m pip install docker textual
+
+# Executar o programa
+python /home/vitor/Projetos/pydocker/PyDocker/src/main.py
